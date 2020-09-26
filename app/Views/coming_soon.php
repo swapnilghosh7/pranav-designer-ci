dfb4b1be52ad78e6bd41815adf9976e3a8016f15
Swapnil Ghosh <swapnilghosh7.com>
2020-09-26 11:48:44 +0530

    Initial Commit

---
+++ app/Views/coming_soon.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,700;0,800;0,900;1,500&display=swap" rel="stylesheet">
+        <style>
+            body{
+                font-family: 'Montserrat', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="websiteComingSoon d-flex h-100 flex-column justify-content-center align-items-center">
+            <h1>pranavdesigner.com</h1>
+            <h2>Website Coming Soon</h2>
+            <h3>Follow me on <a href="https://www.behance.net/PranavPatil">Behance</a></h3>
+        </div>
+    </body>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/core.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script> -->
+</html>
