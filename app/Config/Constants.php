a9def7a28e1325df4129134c57e5fac8da0ce5e3
swapni ghosh <swapnilghosh7@gmail.com>
2020-10-03 04:26:22 +0000

    asswet folder moved

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -76,5 +76,5 @@ defined('EXIT_DATABASE')       || define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      || define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      || define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
-$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://'.$_SERVER['HTTP_HOST'] . '/pranav-designer-ci' : 'http://'.$_SERVER['HTTP_HOST'] . '/pranav-designer-ci';
+$protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://'.$_SERVER['HTTP_HOST']  : 'http://'.$_SERVER['HTTP_HOST'] . '/pranav-designer-ci';
     defined('BASE') || define('BASE',$protocol);
